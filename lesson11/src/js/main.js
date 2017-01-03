5e81fdf32f19b3dcc462a21242682bb06d038bc7
Invisible <invisibl@ukr.net>
2017-01-03 12:24:05 +0200

    header, nav

--- lesson11/src/js/main.js
+++ lesson11/src/js/main.js
@@ -6,6 +6,14 @@ function myFunction() {
         x.className = "topnav";
     }
 }
+$(window).on("scroll", function() {
+    if($(window).scrollTop() > 50) {
+        $(".header").addClass("active");
+    } else {
+        //remove the background property so it comes transparent again (defined in your css)
+        $(".header").removeClass("active");
+    }
+});
 $(document).ready(function(){
     $('.owl-carousel').owlCarousel({
         loop:true,
