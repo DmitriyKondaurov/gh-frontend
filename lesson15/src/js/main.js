74851e181b8e92112d7928f0378c22c3e88dc22c
Kondaurov D <invisibl@ukr.net>
2017-02-01 16:10:08 +0200

    lesson15 template

---
+++ lesson15/src/js/main.js
@@ -0,0 +1,81 @@
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+        $(".menu").removeClass("original");
+        $('.cloned').hide();
+    } else {
+        x.className = "topnav";
+        $(".menu").addClass("original");
+    }
+}
+
+$(window).on("scroll", function() {
+    if($(window).scrollTop() > 50) {
+        $(".header").addClass("active");
+    } else {
+        //remove the background property so it comes transparent again (defined in your css)
+        $(".header").removeClass("active");
+    }
+});
+
+$( "#slide-down" ).click(function () {
+    if ( $( "#analysis-form" ).is( ":hidden" ) ) {
+        $( "#analysis-form" ).slideDown( "slow" );
+    } else {
+        $( "#analysis-form" ).hide( "slow" );
+    }
+});
+
+$( "#close-icon" ).click(function () {
+        $( "#analysis-form" ).hide( "slow" );
+});
+
+
+
+$(document).ready(function(){
+    $('#carousel-1').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:false,
+        items:1,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    })
+});
+// Create a clone of the menu, right next to original.
+$('.menu').addClass('original').clone().insertAfter('.menu').addClass('cloned').css('position','fixed').css('top','0').css('margin-top','0').css('z-index','500').removeClass('original').hide();
+
+scrollIntervalID = setInterval(stickIt, 10);
+
+
+function stickIt() {
+
+    var orgElementPos = $('.original').offset();
+    orgElementTop = orgElementPos.top;
+
+    if ($(window).scrollTop() >= (orgElementTop)) {
+        // scrolled past the original position; now only show the cloned, sticky element.
+
+        // Cloned element should always have same left position and width as original element.
+        orgElement = $('.original');
+        coordsOrgElement = orgElement.offset();
+        leftOrgElement = coordsOrgElement.left;
+        widthOrgElement = orgElement.css('width');
+        $('.cloned').css('left',leftOrgElement+'px').css('top',0).css('width',widthOrgElement).show();
+        $('.original').css('visibility','hidden');
+    } else {
+        // not scrolled past the menu; only show the original menu.
+        $('.cloned').hide();
+        $('.original').css('visibility','visible');
+    }
+}
