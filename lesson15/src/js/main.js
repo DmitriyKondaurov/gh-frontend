d476d3584ac330569b1a98797746613831e94229
Kondaurov D <invisibl@ukr.net>
2017-02-03 22:04:03 +0200

    lesson15 HTML footer ready

--- lesson15/src/js/main.js
+++ lesson15/src/js/main.js
@@ -20,16 +20,19 @@ $(window).on("scroll", function () {
 });
 
 $("#slide-down").click(function () {
-    if ($("#analysis-form").is(":hidden")) {
-        $("#analysis-form").slideDown("slow");
+    if ($(".footer-more").is(":hidden")) {
+        $(".footer-more").show("slow");
+        $('html, body').animate({
+            scrollTop: 6000
+        }, 1000);
     } else {
-        $("#analysis-form").hide("slow");
+        $(".footer-more").hide("slow");
     }
 });
-
-$("#close-icon").click(function () {
-    $("#analysis-form").hide("slow");
-});
+//
+// $("#close-icon").click(function () {
+//     $("#analysis-form").hide("slow");
+// });
 
 
 $(document).ready(function () {
@@ -51,30 +54,4 @@ $(document).ready(function () {
         }
     })
 });
-// Create a clone of the menu, right next to original.
-$('.menu').addClass('original').clone().insertAfter('.menu').addClass('cloned').css('position', 'fixed').css('top', '0').css('margin-top', '0').css('z-index', '500').removeClass('original').hide();
 
-scrollIntervalID = setInterval(stickIt, 10);
-
-
-function stickIt() {
-
-    var orgElementPos = $('.original').offset();
-    orgElementTop = orgElementPos.top;
-
-    if ($(window).scrollTop() >= (orgElementTop)) {
-        // scrolled past the original position; now only show the cloned, sticky element.
-
-        // Cloned element should always have same left position and width as original element.
-        orgElement = $('.original');
-        coordsOrgElement = orgElement.offset();
-        leftOrgElement = coordsOrgElement.left;
-        widthOrgElement = orgElement.css('width');
-        $('.cloned').css('left', leftOrgElement + 'px').css('top', 0).css('width', widthOrgElement).show();
-        $('.original').css('visibility', 'hidden');
-    } else {
-        // not scrolled past the menu; only show the original menu.
-        $('.cloned').hide();
-        $('.original').css('visibility', 'visible');
-    }
-}
