d5e62c0bab9063e942621cb98c6e390466c6b610
Kondaurov D <invisibl@ukr.net>
2017-02-09 11:47:08 +0200

    add html (lesson16/part-1)

--- lesson16/part-1/src/js/main.js
+++ lesson16/part-1/src/js/main.js
@@ -52,21 +52,7 @@ $('.isotope').isotope({
 
 $(document).ready(function () {
     $('#carousel-1').owlCarousel({
-        loop: true,
-        margin: 10,
-        nav: false,
-        items: 1,
-        responsive: {
-            0: {
-                items: 1
-            },
-            600: {
-                items: 1
-            },
-            1000: {
-                items: 1
-            }
-        }
+
     })
 });
 
