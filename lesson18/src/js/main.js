2a129022a6687a0b82133f780488ef1b270745c6
Kondaurov D <invisibl@ukr.net>
2017-03-03 09:19:05 +0200

    bourbon + preloader (lesson18)

--- lesson18/src/js/main.js
+++ lesson18/src/js/main.js
@@ -1,7 +1,13 @@
-$(document).ready(function () {
-    $(".main_head").css("min-height", $(window).height());
+$(document).ready(function() {
+    function heightDetect() {
+        $(".main_head").css("height", $(window).height());
+    };
+    heightDetect();
+    $(window).resize(function() {
+        heightDetect();
+    });
 });
-$(window).load(function() {
+$(window).on('load',function() {
 
     $(".loader_inner").fadeOut();
     $(".loader").delay(400).fadeOut("slow");
@@ -9,13 +15,4 @@ $(window).load(function() {
     $(".top_text h1").animated("fadeInDown", "fadeOutUp");
     $(".top_text p").animated("fadeInUp", "fadeOutDown");
 
-});
-
-$(window).on("scroll", function () {
-    if ($(window).scrollTop() > 50) {
-        $(".header").addClass("active");
-    } else {
-        //remove the background property so it comes transparent again (defined in your css)
-        $(".header").removeClass("active");
-    }
 });
