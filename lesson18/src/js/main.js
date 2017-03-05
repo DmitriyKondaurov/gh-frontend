055f6bf7b75b5ca7f6ab0b7a4aafd37a712d84d2
Kondaurov D <invisibl@ukr.net>
2017-03-05 03:28:40 +0200

    hero (lesson18)

--- lesson18/src/js/main.js
+++ lesson18/src/js/main.js
@@ -8,16 +8,39 @@ $(document).ready(function() {
         heightDetect();
     });
     //parallax image
-    $('.main_head').parallax({imageSrc: '../img/bg.jpg'});
+    $(".hero").parallax({imageSrc: '../img/bg.jpg'});
+    //scroll background
+    $(window).on("scroll", function() {
+        if($(window).scrollTop() > 50) {
+            $("#top_header").addClass("active");
+        } else {
+            //remove the background property so it comes transparent again (defined in your css)
+            $("#top_header").removeClass("active");
+        }
+    });
     //sandwich icon
     $(".toggle_mnu").click(function() {
         $("#sandwich").toggleClass("active");
+        if($("#top_header").hasClass("active")) {
+            $("#top_header").removeClass("active");
+        }
     });
     $(".toggle_mnu").click(function () {
-        if($("#sandwich").hasClass("active"))
+        if($("#sandwich").hasClass("active")) {
             $(".top_mnu").fadeIn(600);
-        else $(".top_mnu").fadeOut(600);
+            $(".top_mnu li a").removeClass("fadeOutUp animated");
+            $(".top_mnu li a").addClass("fadeInUp animated");
+        }
+        else {
+            $(".top_mnu").fadeOut(600);
+            $(".top_mnu li a").addClass("fadeOutUp animated");
+        }
+
     });
+    $(".top_mnu li a").click(function () {
+        $(".top_mnu").fadeOut(600);
+        $("#sandwich").toggleClass("active");
+    })
 });
 //preloader
 $(window).on('load',function() {
