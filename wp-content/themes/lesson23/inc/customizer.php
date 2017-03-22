3711c842bd07a309952a0d3a965c0e78b00b64a2
Kondaurov D <invisibl@ukr.net>
2017-03-22 11:41:05 +0200

    add theme underscore lesson23

---
+++ wp-content/themes/lesson23/inc/customizer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * lesson23 Theme Customizer
+ *
+ * @package lesson23
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function lesson23_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+}
+add_action( 'customize_register', 'lesson23_customize_register' );
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function lesson23_customize_preview_js() {
+	wp_enqueue_script( 'lesson23_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+}
+add_action( 'customize_preview_init', 'lesson23_customize_preview_js' );
